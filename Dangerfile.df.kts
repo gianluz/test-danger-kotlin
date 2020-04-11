ca925a6bbeda5039a92a916eae63e6a929a5c4e3
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-04-11 15:03:04 +0100

    Test

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,26 +1,28 @@
-//@file:DependsOn("com.danger:danger-kotlin-library:0.1.0")
 @file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.0.3")
 
 import com.gianluz.dangerkotlin.androidlint.AndroidLint
-import systems.danger.kotlin.Danger
+import systems.danger.kotlin.danger
 import systems.danger.kotlin.register
 import systems.danger.kotlin.warn
 
 register plugin AndroidLint
 
-val danger = Danger(args)
+danger(args) {
 
-val allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+    warn("args $args")
 
-val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
-val isTrivial = danger.github.pullRequest.title.contains("#trivial")
+    val allSourceFiles = git.modifiedFiles + git.createdFiles
 
-if (danger.git.createdFiles.size + danger.git.modifiedFiles.size - danger.git.deletedFiles.size > 10) {
-    warn("Big PR, try to keep changes smaller if you can")
+    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
+    val isTrivial = github.pullRequest.title.contains("#trivial")
+
+    if (git.createdFiles.size + git.modifiedFiles.size - git.deletedFiles.size > 10) {
+        warn("Big PR, try to keep changes smaller if you can")
+    }
+
+    if (github.pullRequest.title.contains("WIP", false)) {
+        warn("PR is classed as Work in Progress")
+    }
+
+    AndroidLint.report("app/build/reports/lint-results.xml")
 }
-
-if (danger.github.pullRequest.title.contains("WIP", false)) {
-    warn("PR is classed as Work in Progress")
-}
-
-AndroidLint.report("./app/build/reports/lint-results.xml")
