b06c31250a823f018de0475c474727bbd8bcca7a
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-04-19 13:52:12 +0100

    Test

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,9 +1,7 @@
-@file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.0.3")
+@file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.1.0")
 
-import com.gianluz.dangerkotlin.androidlint.AndroidLint
-import systems.danger.kotlin.danger
-import systems.danger.kotlin.register
-import systems.danger.kotlin.warn
+import com.gianluz.dangerkotlin.androidlint.*
+import systems.danger.kotlin.*
 
 register plugin AndroidLint
 
@@ -12,9 +10,8 @@ danger(args) {
     val argsString = buildString {
         args.forEach { append(it).append(" ") }
     }
-    warn("Bitrise test : args [$argsString]")
 
-    val allSourceFiles = git.modifiedFiles + git.createdFiles
+    warn("Bitrise test : args [$argsString]")
 
     if (git.createdFiles.size + git.modifiedFiles.size - git.deletedFiles.size > 10) {
         warn("Big PR, try to keep changes smaller if you can")
@@ -24,5 +21,7 @@ danger(args) {
         warn("PR is classed as Work in Progress")
     }
 
-    AndroidLint.report("./app/build/reports/lint-results-debug.xml")
+    androidLint {
+        report("./app/build/reports/lint-results-debug.xml")
+    }
 }
